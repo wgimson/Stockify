d31b79455859d4e19caadbc42873c035570ed5ac
wgimson <wgimson@gmail.com>
2017-08-07 17:31:21 -0400

    added simple table building loop - BG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works - test!';
+export class AppComponent implements OnInit {
+  rows: string[] = ['hello', 'goodbye', 'tallyho'];
+
+  ngOnInit() {
+
+  }
 }
